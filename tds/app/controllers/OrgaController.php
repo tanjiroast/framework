1ce7e03c5bf439a3eb9c26a75f85aaf43ebebcad
Gaëlle <21902643@etu.unicaen.fr>
2021-02-14 20:07:25 +0100

    ajout initialize (bon)

--- tds/app/controllers/OrgaController.php
+++ tds/app/controllers/OrgaController.php
@@ -4,11 +4,14 @@ namespace controllers;
  use models\Organization;
  use Ubiquity\attributes\items\router\Route;
  use Ubiquity\orm\DAO;
+ use Ubiquity\orm\repositories\ViewRepository;
 
  /**
  * Controller OrgaController
  **/
 class OrgaController extends ControllerBase{
+
+    private ViewRepository $repo;
 
     public function initialize(){
         parent::initialize();
