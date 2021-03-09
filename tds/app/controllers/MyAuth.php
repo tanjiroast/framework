1ff7f7184bf8852cbdacf10b6b852ee4b7a365dd
Gaëlle <21902643@etu.unicaen.fr>
2021-03-09 17:42:03 +0100

    liaison entre myauth et maincontroller+connexion

---
+++ tds/app/controllers/MyAuth.php
@@ -0,0 +1,86 @@
+<?php
+namespace controllers;
+use models\User;
+use Ubiquity\attributes\items\router\Route;
+use Ubiquity\controllers\Router;
+use Ubiquity\orm\DAO;
+use Ubiquity\utils\flash\FlashMessage;
+use Ubiquity\utils\http\UResponse;
+use Ubiquity\utils\http\USession;
+use Ubiquity\utils\http\URequest;
+use Ubiquity\controllers\auth\AuthFiles;
+
+
+class MyAuth extends \Ubiquity\controllers\auth\AuthController{
+
+    protected function onConnect($connected) {
+        $urlParts=$this->getOriginalURL();
+        USession::set($this->_getUserSessionKey(), $connected);
+        if(isset($urlParts)){
+            $this->_forward(implode("/",$urlParts));
+        }else{
+            //TODO
+            UResponse::header('location','/'.Router::path('menu'));
+        }
+    }
+
+    protected function _connect() {
+        if(URequest::isPost()){
+            $email=URequest::post($this->_getLoginInputName());
+            $password=URequest::post($this->_getPasswordInputName());
+            //TODO
+            //Loading from the database the user corresponding to the parameters
+            //Checking user creditentials
+            //Returning the user
+            if($email != null){
+                $user = DAO::getOne(User::class, 'email= ?',false, [$email]);
+                if (isset($user)){
+                    USession::set('idOrga', $user->getOrganization());
+                    return $user;
+                }
+            }
+            //       return 'jul';
+
+        }
+        return;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \Ubiquity\controllers\auth\AuthController::isValidUser()
+     */
+    public function _isValidUser($action=null) {
+        return USession::exists($this->_getUserSessionKey());
+    }
+
+    public function _getBaseRoute() {
+        return '/login';
+    }
+
+    public function _displayInfoAsString() {
+        return true;
+    }
+
+    protected function finalizeAuth() {
+        if(!URequest::isAjax()){
+            $this->loadView('@activeTheme/main/vFooter.html');
+        }
+    }
+
+    protected function initializeAuth() {
+        if(!URequest::isAjax()){
+            $this->loadView('@activeTheme/main/vHeader.html');
+        }
+    }
+
+    public function _getBodySelector() {
+        return '#page-container';
+    }
+
+    protected function noAccessMessage(FlashMessage $fMessage)
+    {
+        $fMessage->setTitle('Accès interdit');
+        $fMessage->setContent('Vous ne pouvez accèder à cette ressource');
+    }
+
+}
