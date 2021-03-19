e49b44e0837e543fa6de8533d11067fb7f9964b1
Gaëlle <21902643@etu.unicaen.fr>
2021-03-19 13:52:13 +0100

    Connexion et déconnexion réussies

--- tds/app/controllers/MainController.php
+++ tds/app/controllers/MainController.php
@@ -1,15 +1,23 @@
 <?php
 namespace controllers;
 use Ubiquity\attributes\items\router\Route;
+use Ubiquity\controllers\auth\AuthController;
+use Ubiquity\controllers\auth\WithAuthTrait;
 
- /**
+/**
  * Controller MainController
  **/
 class MainController extends ControllerBase{
+
+    use WithAuthTrait;
 
     #[Route ('_default', name:'home')]
 	public function index(){
         $this->loadDefaultView();
     }
 
+    protected function getAuthController(): AuthController
+    {
+        return new MyAuth($this);
+    }
 }
--- tds/app/controllers/MyAuth.php
+++ tds/app/controllers/MyAuth.php
@@ -1,48 +1,60 @@
 <?php
 namespace controllers;
-use models\User;
+use Ubiquity\utils\http\USession;
+use Ubiquity\utils\http\URequest;
+use controllers\auth\files\MyAuthFiles;
+use Ubiquity\controllers\auth\AuthFiles;
 use Ubiquity\attributes\items\router\Route;
+use Ubiquity\attributes\items\router\Get;
 use Ubiquity\controllers\Router;
+use Ubiquity\controllers\Startup;
 use Ubiquity\orm\DAO;
 use Ubiquity\utils\flash\FlashMessage;
 use Ubiquity\utils\http\UResponse;
-use Ubiquity\utils\http\USession;
-use Ubiquity\utils\http\URequest;
-use Ubiquity\controllers\auth\AuthFiles;
+use models\User;
 
-
+#[Route(path: "/login",inherited: true,automated: true)]
 class MyAuth extends \Ubiquity\controllers\auth\AuthController{
 
     protected function onConnect($connected) {
         $urlParts=$this->getOriginalURL();
         USession::set($this->_getUserSessionKey(), $connected);
         if(isset($urlParts)){
-            $this->_forward(implode("/",$urlParts));
-        }else{
-            //TODO
-            UResponse::header('location','/'.Router::path('menu'));
+            $url=implode("/",$urlParts);
+            if(!str_contains($url, 'direct')) {
+                //return $this->_forward(implode("/", $urlParts));
+            }
         }
+        UResponse::header('location','/');
+    }
+
+    #[Get(name:'login.direct')]
+    public function direct($name){
+        $name=urldecode($name);
+        $user = DAO::getOne(User::class, 'email= ?', false, [$name]);
+        if($user) {
+            return $this->onConnect($user);
+        }
+        $this->_invalid=true;
+        $this->initializeAuth();
+        $this->onBadCreditentials ();
+        $this->finalizeAuth();
     }
 
     protected function _connect() {
         if(URequest::isPost()){
             $email=URequest::post($this->_getLoginInputName());
-            $password=URequest::post($this->_getPasswordInputName());
-            //TODO
-            //Loading from the database the user corresponding to the parameters
-            //Checking user creditentials
-            //Returning the user
-            if($email != null){
-                $user = DAO::getOne(User::class, 'email= ?',false, [$email]);
-                if (isset($user)){
-                    USession::set('idOrga', $user->getOrganization());
+            if($email!=null) {
+                $password = URequest::post($this->_getPasswordInputName());
+                $user = DAO::getOne(User::class, 'email= ?', false, [$email]);
+                if(isset($user)) {
                     return $user;
                 }
             }
-            //       return 'jul';
-
         }
+
         return;
+
     }
 
     /**
@@ -57,18 +69,22 @@ class MyAuth extends \Ubiquity\controllers\auth\AuthController{
         return '/login';
     }
 
+    protected function getFiles(): AuthFiles{
+        return new MyAuthFiles();
+    }
+
     public function _displayInfoAsString() {
         return true;
     }
 
     protected function finalizeAuth() {
-        if(!URequest::isAjax()){
+        if(!URequest::isAjax() && Startup::getAction()!=='direct'){
             $this->loadView('@activeTheme/main/vFooter.html');
         }
     }
 
     protected function initializeAuth() {
-        if(!URequest::isAjax()){
+        if(!URequest::isAjax() && Startup::getAction()!=='direct'){
             $this->loadView('@activeTheme/main/vHeader.html');
         }
     }
@@ -77,10 +93,17 @@ class MyAuth extends \Ubiquity\controllers\auth\AuthController{
         return '#page-container';
     }
 
-    protected function noAccessMessage(FlashMessage $fMessage)
-    {
-        $fMessage->setTitle('Accès interdit');
-        $fMessage->setContent('Vous ne pouvez accèder à cette ressource');
+    protected function noAccessMessage(FlashMessage $fMessage) {
+        $fMessage->setTitle('Accès interdit !');
+        $u=USession::get('urlParts');
+        $url=implode('/',$u);
+        $url=($url=='_default')?'/':$url;
+        $fMessage->setContent("Vous n'êtes pas autorisé à accéder à cette page (".$url.").");
+    }
+
+    protected function terminateMessage(FlashMessage $fMessage) {
+        $fMessage->setTitle('Fermeture');
+        $fMessage->setContent("Vous avez été correctement déconnecté de l'application.");
     }
 
 }
